9c69a7af974c18207e9a4cab3eb2fe4bd929751b
Nikhil-Pahwa <nikhil.pahwa@nagarro.com>
2016-08-26 01:15:04 +0530

    minor fixes

--- Angular2-GettingStarted-master/Angular2-GettingStarted-master/APM - Start VS 2015 ASP 4x/APMSolution/APM/app/app.component.ts
+++ Angular2-GettingStarted-master/Angular2-GettingStarted-master/APM - Start VS 2015 ASP 4x/APMSolution/APM/app/app.component.ts
@@ -1,2 +1,15 @@
-﻿export class AppComponent {
+﻿import {Component} from "angular2/core";
+import {ProductListComponent} from './product/product-list.component';
+
+@Component({
+    selector: "pm-am",
+    template: `
+        <div> {{pageTitle}} </div>
+        <pm-products></pm-products>
+    `,
+    directives: [ProductListComponent]
+})
+
+export class AppComponent {
+    pageTitle: String = "My Default Project";
 }
